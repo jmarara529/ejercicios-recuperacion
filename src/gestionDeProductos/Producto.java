35700d6b83b84d4092c9ddb282af6f587f6b9265
jmarara529 <jmarara529@g.educaand.es>
2024-06-17 10:34:02 +0200

    Producto

---
+++ src/gestionDeProductos/Producto.java
@@ -0,0 +1,48 @@
+package gestionDeProductos;
+
+import java.io.Serializable;
+
+public class Producto implements Serializable {
+    private String marca;
+    private String descripcion;
+    private float precio;
+
+    public Producto(String marca, String descripcion, float precio) {
+        this.marca = marca;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "marca='" + marca + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+}
